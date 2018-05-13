e41b4d11c818e709603d9a068d726989c6940049
GLYASAI <chaoschunqfeijg@163.com>
2018-05-13 22:16:13 +0800

    完全二叉树的节点数

---
+++ src/main/java/org/abewang/iq/binarytree/CBTNodeQuantity.java
@@ -0,0 +1,41 @@
+package org.abewang.iq.binarytree;
+
+/**
+ * 完全二叉树的节点数
+ *
+ * @Author Abe
+ * @Date 2018/5/13.
+ */
+public class CBTNodeQuantity {
+    public static Integer countCBTNode(Node node, int level, int height) {
+        if (level == height) {
+            return 1;
+        }
+
+        if (getMostLeftLevel(node.right, level + 1) == height) {
+            return (1 << (height - level)) + countCBTNode(node.right, level + 1, height);
+        } else {
+            return (1 << (height - level - 1))+ countCBTNode(node.left, level + 1, height);
+        }
+    }
+
+    public static Integer getMostLeftLevel(Node node, int level) {
+        while (node != null && node.left != null) {
+            level++;
+            node = node.left;
+        }
+
+        return level;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.left = new Node(2);
+        head.right = new Node(3);
+        head.left.left = new Node(4);
+        head.left.right = new Node(5);
+        head.right.left = new Node(6);
+        head.right.right = new Node(7);
+        System.out.println(countCBTNode(head, 1, getMostLeftLevel(head, 1)));
+    }
+}
