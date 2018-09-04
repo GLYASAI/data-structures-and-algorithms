22fc197779848e38db98123d03909ade13d0b5bb
GLYASAI <chaoschunqfeijg@163.com>
2018-09-04 19:32:40 +0800

    求一个整数中二进制1的个数

---
+++ src/main/java/org/abewang/dsaa/iq/foobar/NumberOfOne.java
@@ -0,0 +1,23 @@
+package org.abewang.dsaa.iq.foobar;
+
+/**
+ * 求一个整数中二进制1的个数
+ *
+ * @Author Abe
+ * @Date 2018/9/4.
+ */
+public class NumberOfOne {
+    static int getOneNum(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);  // 有多少个1, 就可以进行多少次这个操作.
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getOneNum(0));
+    }
+}
