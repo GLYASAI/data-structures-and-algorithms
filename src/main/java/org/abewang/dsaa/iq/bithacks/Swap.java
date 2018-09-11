4c18a7dbd4fff0f85cf0970f53123147dd1886e4
GLYASAI <chaoschunqfeijg@163.com>
2018-09-11 10:25:51 +0800

    swap two integers without using any third variable

---
+++ src/main/java/org/abewang/dsaa/iq/bithacks/Swap.java
@@ -0,0 +1,26 @@
+package org.abewang.dsaa.iq.bithacks;
+
+/**
+ * swap two integers without using any third variable.
+ *
+ * x ^ x = 0
+ *
+ * @Author Abe
+ * @Date 2018/9/11.
+ */
+public class Swap {
+
+    public static void swap(int a, int b) {
+        System.out.println("before swap: a = " + a + "; b = " + b);
+
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("after swap: a = " + a + "; b = " + b);
+    }
+
+    public static void main(String[] args) {
+        swap(3, 4);
+    }
+}
