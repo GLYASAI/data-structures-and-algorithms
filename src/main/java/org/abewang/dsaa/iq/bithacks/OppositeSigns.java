389662e429083b620e4195fdad6d1a37ec2e1fc7
GLYASAI <chaoschunqfeijg@163.com>
2018-09-11 09:57:40 +0800

    detect if two integers have opposite signs or not

---
+++ src/main/java/org/abewang/dsaa/iq/bithacks/OppositeSigns.java
@@ -0,0 +1,23 @@
+package org.abewang.dsaa.iq.bithacks;
+
+/**
+ * detect if two integers have opposite signs or not.
+ *
+ * @Author Abe
+ * @Date 2018/9/11.
+ */
+public class OppositeSigns {
+    public static void ifHaveOppositeSigns(int a, int b) {
+        if ((a ^ b) < 0) {
+            System.out.println(a + " and " + b + " have opposite signs.");
+        } else {
+            System.out.println(a + " and " + b + " don't have opposite signs.");
+        }
+    }
+
+    public static void main(String[] args) {
+        ifHaveOppositeSigns(4, 8);
+        ifHaveOppositeSigns(4, -8);
+        ifHaveOppositeSigns(-4, -8);
+    }
+}
