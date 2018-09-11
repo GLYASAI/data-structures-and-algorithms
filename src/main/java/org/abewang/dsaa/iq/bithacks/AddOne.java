e65ddafea71b0e55a8b854a547eaf5961f752875
GLYASAI <chaoschunqfeijg@163.com>
2018-09-11 10:14:20 +0800

    +1

---
+++ src/main/java/org/abewang/dsaa/iq/bithacks/AddOne.java
@@ -0,0 +1,22 @@
+package org.abewang.dsaa.iq.bithacks;
+
+/**
+ * Add 1 to a given integer
+ *
+ * -x = ~x + 1, x + 1 = -~x
+ *
+ * @Author Abe
+ * @Date 2018/9/11.
+ */
+public class AddOne {
+
+    public static void addOne(int a) {
+        System.out.println(a + " add 1 is " + -~a);
+    }
+
+    public static void main(String[] args) {
+        addOne(5);
+        addOne(-5);
+        addOne(0);
+    }
+}
